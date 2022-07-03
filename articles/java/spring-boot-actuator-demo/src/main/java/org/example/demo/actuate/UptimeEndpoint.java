8bf0eecf07fa2b95d6059f552e46239f02a6d9e2
Plamen Totev <plamen.iv.totev@gmail.com>
2022-07-03 10:34:25 +0300

    Add Implement Custom Spring Boot Actuator Endpoint blog post

---
+++ articles/java/spring-boot-actuator-demo/src/main/java/org/example/demo/actuate/UptimeEndpoint.java
@@ -0,0 +1,22 @@
+package org.example.demo.actuate;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.lang.management.ManagementFactory;
+
+@Component
+@Endpoint(id = "uptime") //<1>
+public class UptimeEndpoint {
+
+    @ReadOperation //<2>
+    public Uptime getUptime() {
+        var runtime = ManagementFactory.getRuntimeMXBean();
+
+        return new Uptime(runtime.getUptime(), runtime.getStartTime());
+    }
+
+    private record Uptime(long uptime, long startTime) { }
+
+}
---
+++ articles/java/spring-boot-actuator-demo/src/test/java/org/example/demo/SpringBootActuatorDemoTest.java
@@ -0,0 +1,38 @@
+package org.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
+public class SpringBootActuatorDemoTest {
+
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+			.apply(documentationConfiguration(restDocumentation))
+			.build();
+	}
+
+	@Test
+	void getCurrentTimeEndpoint() throws Exception {
+		this.mockMvc.perform(get("/actuator/uptime"))
+			.andExpect(status().isOk())
+			.andDo(document("uptime"));
+	}
+
+}
